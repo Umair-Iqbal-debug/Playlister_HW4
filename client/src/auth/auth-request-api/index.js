23b80c622f73aaacdb1da0d5e70e8b10eb55aee6
Umair Iqbal <iqbalumairi@gmail.com>
2022-11-02 11:20:58 -0400

    CLIENT: Fixed status-bar css

--- client/src/auth/auth-request-api/index.js
+++ client/src/auth/auth-request-api/index.js
@@ -24,6 +24,7 @@ const api = axios.create({
 // CUSTOM FILTERS FOR QUERIES
 
 export const getLoggedIn = () => api.get(`/loggedIn/`);
+
 export const loginUser = (email, password) => {
     return api.post(`/login/`, {
         email : email,
